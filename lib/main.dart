646fd46c722c021c606d9fac2c20b8c6e6d66128
Dwayne Belcon <evo@MyMacs-MacBook-Pro.local>
2019-11-30 11:26:46 -0500

    Initial push to GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,137 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange[900],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80.0,
+                backgroundImage: AssetImage('images/itsame.jpeg'),
+              ),
+              Text(
+                'Dwayne Belcon',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FULL STACK DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Tomorrow'),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.orange[900],
+                      size: 35.0,
+                    ),
+                    title: Text(
+                      '888-817-2300',
+                      style: TextStyle(
+                          color: Colors.orange.shade900,
+                          fontSize: 22.0,
+                          fontFamily: 'Tomorrow'
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                  leading: Icon(
+                  Icons.email,
+                  color: Colors.orange[900],
+                  size: 35.0,
+                ),
+                title: Text(
+                  'dmbelcon@evodev.com',
+                  style: TextStyle(
+                      color: Colors.orange.shade900,
+                      fontSize: 22.0,
+                      fontFamily: 'Tomorrow'
+                  ),
+                ),
+              ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.person,
+                      color: Colors.orange[900],
+                      size: 35.0,
+                    ),
+                    title: Text(
+                      'github.com/Evofinis',
+                      style: TextStyle(
+                          color: Colors.orange.shade900,
+                          fontSize: 22.0,
+                          fontFamily: 'Tomorrow'
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.photo_camera,
+                      color: Colors.orange[900],
+                      size: 35.0,
+                    ),
+                    title: Text(
+                      '@evodnd',
+                      style: TextStyle(
+                          color: Colors.orange.shade900,
+                          fontSize: 22.0,
+                          fontFamily: 'Tomorrow'
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
